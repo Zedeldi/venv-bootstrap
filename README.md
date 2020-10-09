0e59acc59c41308e351a2e6e641edfeaa293151e
Zedeldi <66186954+Zedeldi@users.noreply.github.com>
2020-10-09 22:34:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# venv-bootstrap
+
+[![GitHub license](https://img.shields.io/github/license/Zedeldi/venv-bootstrap?style=flat-square)](https://github.com/Zedeldi/venv-bootstrap/blob/master/LICENSE) [![GitHub last commit](https://img.shields.io/github/last-commit/Zedeldi/venv-bootstrap?style=flat-square)](https://github.com/Zedeldi/venv-bootstrap/commits)
+
+Small Python script to bootstrap a virtual environment, with specified packages.
+
+## Description
+
+venv-bootstrap creates a [virtual environment](https://docs.python.org/3/library/venv.html), and installs the specified packages using [pip](https://pypi.org/project/pip/). Only the [standard library](https://docs.python.org/3/library/index.html) is required; this project was designed to be portable and implemented by other projects to help automate their setup process.
+
+It accepts a space-delimited list of packages and/or requirement files. The [`venv.EnvBuilder.post_setup()`](https://docs.python.org/3/library/venv.html#venv.EnvBuilder.post_setup) method can also be overridden to perform other post-creation steps.
+
+## Installation
+
+1. Clone this repo: `git clone https://github.com/Zedeldi/venv-bootstrap.git`
+2. Run: `python3 venv_bootstrap.py -r requirements.txt -p "foo bar baz" .venv`
+
+### Usage
+
+To create a venv from a different project, use the following:
+
+```python
+from venv_bootstrap import create_venv, parse_requirements
+
+create_venv(".venv", parse_requirements(["requirements.txt"]))
+```
+
+## Todo
+
+- Cross-platform support
+
+## License
+
+venv-bootstrap is licensed under the GPL v3 for everyone to use, modify and share freely.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+[![GPL v3 Logo](https://www.gnu.org/graphics/gplv3-127x51.png)](https://www.gnu.org/licenses/gpl-3.0-standalone.html)
+
+## Donate
+
+If you found this project useful, please consider donating. Any amount is greatly appreciated! Thank you :smiley:
+
+My bitcoin address is: [bc1q5aygkqypxuw7cjg062tnh56sd0mxt0zd5md536](bitcoin://bc1q5aygkqypxuw7cjg062tnh56sd0mxt0zd5md536)
---
+++ venv_bootstrap.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+# venv_bootstrap.py
+# Copyright (C) 2020  Zack Didcott
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+Bootstrap a venv with required packages
+
+TODO
+----
+Cross-platform support
+"""
+
+import logging, os, venv
+from argparse import ArgumentParser
+from shutil import make_archive
+from subprocess import check_call, CalledProcessError, DEVNULL, STDOUT
+
+def create_venv(path, packages=[]):
+	"""
+	Create venv and install specified packages
+	
+	See https://docs.python.org/3/library/venv.html for more information
+	"""
+	venv.create(path, clear=True, symlinks=False, with_pip=True)
+	
+	if os.name == 'nt': python = os.path.join(path, "Scripts", "python.exe")
+	else: python = os.path.join(path, "bin", "python")
+	
+	for pkg in packages:
+		try:
+			logging.info("Installing {0}...".format(pkg))
+			check_call([python, "-m", "pip", "install", pkg], stdout=DEVNULL, stderr=STDOUT)
+		except CalledProcessError:
+			logging.warning("{0} install failed.".format(pkg))
+
+def parse_requirements(requirements):
+	"""Read requirement files and return list of packages"""
+	packages = []
+	for req in requirements:
+		try:
+			for line in open(req, 'r').readlines():
+				pkg = line.strip()
+				if not pkg.startswith("#"):
+					packages.append(pkg)
+			# Alternatively, let pip handle requirements
+			#open(req, 'r').close()
+			#PACKAGES.append("-r" + req)
+		except OSError:
+			logging.warning("{0} not found.".format(req))
+	return packages
+
+def archive(path):
+	"""Create zip archive of path"""
+	logging.info("Compressing...")
+	archive = make_archive(path, "zip", path)
+	return archive
+
+if __name__ == "__main__":
+	parser = ArgumentParser(description="venv_bootstrap - Copyright (C) 2020 Zack Didcott")
+	parser.add_argument("directory", type=str, help="path for the virtual environment")
+	parser.add_argument("-p", "--packages", type=str, default="", help="space-separated string of packages")
+	parser.add_argument("-r", "--requirements", type=str, default="", help="space-separated string of requirements files")
+	parser.add_argument("-l", "--log", type=str, default=None, help="output log into a file")
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument("-q", "--quiet", action="store_const", const=-1, default=0, dest="verbosity", help="only display warnings")
+	group.add_argument("-v", "--verbose", action="store_const", const=1, default=0, dest="verbosity", help="display debugging information")
+
+	args = parser.parse_args()
+	
+	loglevel = 20 - (args.verbosity * 10)
+	logging.basicConfig(
+		level=loglevel,
+		filename=args.log,
+		format="%(asctime)s | [%(levelname)s] %(message)s",
+		datefmt="%H:%M:%S"
+	)
+	
+	logging.info(parser.description)
+	logging.debug("== Configuration ==")
+	for arg, value in sorted(vars(args).items()):
+		logging.debug("{0}: {1}".format(arg.title(), value))
+	logging.debug("===================")
+
+	DIRECTORY = args.directory
+	PACKAGES = args.packages.split()
+	REQUIREMENTS = args.requirements.split()
+	PACKAGES.extend(parse_requirements(REQUIREMENTS))
+	
+	create_venv(DIRECTORY, PACKAGES)
+	archive(DIRECTORY)
+	logging.info("Done! :)")
