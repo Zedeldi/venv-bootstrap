ab703d2727a6aeace57a8b5960f9921ba947dc35
Zedeldi <66186954+Zedeldi@users.noreply.github.com>
2020-11-29 20:52:33 +0000

    Creation of zip archive is now optional

--- venv_bootstrap.py
+++ venv_bootstrap.py
@@ -80,6 +80,7 @@ if __name__ == "__main__":
 	parser.add_argument("-p", "--packages", type=str, default="", help="space-separated string of packages")
 	parser.add_argument("-r", "--requirements", type=str, default="", help="space-separated string of requirements files")
 	parser.add_argument("-o", "--pipopts", type=str, default="", help="additional arguments to pass to pip")
+	parser.add_argument("-z", "--zip", action="store_true", default=False, help="create zip archive of venv")
 	parser.add_argument("-l", "--log", type=str, default=None, help="output log into a file")
 	group = parser.add_mutually_exclusive_group()
 	group.add_argument("-q", "--quiet", action="store_const", const=-1, default=0, dest="verbosity", help="only display warnings")
@@ -108,7 +109,7 @@ if __name__ == "__main__":
 	PACKAGES.extend(parse_requirements(REQUIREMENTS))
 	
 	err = create_venv(DIRECTORY, PACKAGES, PIP_OPTS)
-	archive(DIRECTORY)
+	if args.zip: archive(DIRECTORY)
 	if err:
 		padding = "\n{0:>13}".format(' ')
 		logging.warning("The following packages failed to install:{0}{1}".format(
